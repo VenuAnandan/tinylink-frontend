8fd95d11f4eb6124a1e43e724c38431efc4090c3
VenuAnandan <142042622+VenuAnandan@users.noreply.github.com>
2025-11-22 08:08:13 -0800

    Add database health check function to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,13 @@ import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 export default function Navbar() {
+  
+  const API = "https://tinylink-backend-l4xr.onrender.com"
+  const healthcheckfunction=async()=>{
+    const resposne = await axios.get(`${API}/healthz`);
+        alert( "Database Connection : "+resposne.data.database);
+  }
+  
   return (
     <nav className="nav">
       <h1 className="nav-title">TinyLink</h1>
@@ -9,6 +16,7 @@ export default function Navbar() {
       <div className="nav-links">
         <Link to="/" className="nav-link">Dashboard</Link>
         <Link to="/add" className="nav-link">Add New</Link>
+        <p className="nav-link" onClick={()=>healthcheckfunction()}>Check DB Health</p>
       </div>
     </nav>
   );
